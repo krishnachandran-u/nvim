5998771852e5c3fd54667d4a38e162f6b1c57e32
krishnachandran-u <kriss52450@gmail.com>
2024-02-12 11:33:07 +0530

    yayy made copy to clipboard work

--- lua/clipboard.lua
+++ lua/clipboard.lua
@@ -1,3 +1,4 @@
+--[[
 -- Copy to clipboard
 vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>Y', '"+yg_', { noremap = true })
@@ -9,4 +10,11 @@ vim.api.nvim_set_keymap('n', '<leader>p', '"+p', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>P', '"+P', { noremap = true })
 vim.api.nvim_set_keymap('v', '<leader>p', '"+p', { noremap = true })
 vim.api.nvim_set_keymap('v', '<leader>P', '"+P', { noremap = true })
+]]
+
+-- Set clipboard to system clipboard
+-- Install xclip to use system clipboard
+-- sudo apt-get install xclip
+
+vim.api.nvim_command('set clipboard=unnamedplus')
 
