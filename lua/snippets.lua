5d4b8e42d8b3b4704d0db13186e973d0f42518fd
Krishnachandran U <kriss52450@gmail.com>
2024-04-22 07:58:23 +0530

    set <C-H> and <C-L> for snippet jump

--- lua/snippets.lua
+++ lua/snippets.lua
@@ -12,7 +12,7 @@ local fmta = require("luasnip.extras.fmt").fmta
 
 vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
 vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
-vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-H>", function() ls.jump(-1) end, {silent = true})
 
 vim.keymap.set({"i", "s"}, "<C-E>", function()
 	if ls.choice_active() then
