7980bf3a7a57a4788a60a100d111143854cb3281
Krishnachandran U <kriss52450@gmail.com>
2024-07-02 21:31:39 +0530

    install more lsps, code restructuring

--- lua/mason-lsp.lua
+++ lua/mason-lsp.lua
@@ -1,13 +1,22 @@
 require("mason").setup()
+
+local lsp_servers = {
+    "clangd",
+    "pyright", 
+    "bashls", 
+    "gopls",
+    "dockerls",
+    "docker_compose_language_service",
+    "lua_ls"
+}
+
 require("mason-lspconfig").setup({
-    ensure_installed = { "clangd", "pyright" },
+    ensure_installed = lsp_servers,
 })
 
+local lspconfig = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-require("lspconfig").clangd.setup {
-    capabilities = capabilities,
-}
-require("lspconfig").pyright.setup {
-    capabilities = capabilities,
-}
+for _, lsp in ipairs(lsp_servers) do
+    lspconfig[lsp].setup({ capabilities = capabilities })
+end
